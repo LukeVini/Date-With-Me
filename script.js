f8376167bfa3a5004fef6b57be2b1013687ec0a4
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-07-26 16:13:52 -0300

    Files

---
+++ script.js
@@ -0,0 +1,48 @@
+function Alerta() {
+    document.getElementById('butao').id = 'esconderbtn';
+    document.getElementById('butao').id = 'esconderbtn';
+}
+
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g, '')
+    value = value.replace(/(\d{2})(\d)/, "($1) $2")
+    value = value.replace(/(\d)(\d{4})$/, "$1-$2")
+    return value
+}
+
+const button = document.getElementById("runaway-btn");
+
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc"
+  });
+
+["mouseover", "click"].forEach(function (el) {
+  button.addEventListener(el, function (event) {
+    const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+    const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+
+    animateMove(this, "left", left).play();
+    animateMove(this, "top", top).play();
+  });
+});
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
+
+function Sim(){
+    document.getElementById('aceitas').classList.add('esconder');
+    document.getElementById('tbn-punheta').classList.add('esconder');
+    document.getElementById('runaway-btn').classList.add('esconder');
+    document.querySelector("img").classList.remove("esconder");
+
+}
